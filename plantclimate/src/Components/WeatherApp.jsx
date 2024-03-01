03e2022f2069bd928d159741918fa026f8676305
rin-11 <erinkates11@gmail.com>
2024-03-01 16:45:38 -0500

    search styling

--- plantclimate/src/Components/WeatherApp.jsx
+++ plantclimate/src/Components/WeatherApp.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from 'react';
+import './WeatherApp.css';
+import search_icon from './Assets/search.png';
+import clear_icon from './Assets/clear.png';
+import rain_icon from './Assets/rain.png';
+import wind_icon from './Assets/wind.png';
+import snow_icon from './Assets/snow.png';
+import clouds_icon from './Assets/cloud.png';
+import drizzle_icon from './Assets/drizzle.png';
+import humidity_icon from './Assets/humidity.png';
 
 export const WeatherApp = () => {
   return (
-    <div>WeatherApp</div>
+    <div className="weather_container">
+        <div className="top-bar">
+                <input type="text" className="cityInput" placeholder="Search" />
+                <div className="search-icon">
+                    <img src={search_icon} alt="search" className="search" />
+                </div>     
+        </div>
+    </div>
   )
 }
+
+export default WeatherApp;
