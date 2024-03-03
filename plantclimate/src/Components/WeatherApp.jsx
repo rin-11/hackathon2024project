c6c5c552a58c052afb105ba9a73fe4f40b659cb4
rin-11 <erinkates11@gmail.com>
2024-03-03 12:34:56 -0500

    weather icons

--- plantclimate/src/Components/WeatherApp.jsx
+++ plantclimate/src/Components/WeatherApp.jsx
@@ -12,6 +12,8 @@ import humidity_icon from './Assets/humidity.png';
 export const WeatherApp = () => {
     let api_key = "bbe31ca2e8e78841c19c7c154ed245e7";
 
+    const [weathericon, setWeatherIcon] = React.useState("clouds_icon");
+
     const search = async () => {
         // get the search element
         const element = document.getElementsByClassName("cityInput");
@@ -25,18 +27,47 @@ export const WeatherApp = () => {
         let response = await fetch(url);
         let data = await response.json();
 
+        // update weather elements
         if(data && data.main) {
             const humidity = document.getElementsByClassName("humidity-percent");
             const wind = document.getElementsByClassName("wind-rate");
             const temperature = document.getElementsByClassName("weather-temp");
             const location = document.getElementsByClassName("weather-location");
+            const feelsLike = document.getElementsByClassName("feels-like");
 
             humidity[0].innerHTML = data.main.humidity + "%";
             wind[0].innerHTML = data.wind.speed + " km/hr";
             temperature[0].innerHTML = Math.round(data.main.temp) + "°F";
+            feelsLike[0].innerHTML = "Feels Like " + Math.round(data.main.feels_like) + "°F";
             location[0].innerHTML = data.name;
         } else {
             console.error("Failed to retrieve weather data:", data);
+        }
+
+        // set weather icon
+        if(data.weather[0].icon === "01d" || data.weather[0].icon === "01n"){
+            setWeatherIcon(clear_icon);
+        }
+        else if(data.weather[0].icon === "02d" || data.weather[0].icon === "02n"){
+            setWeatherIcon(clouds_icon);
+        }
+        else if(data.weather[0].icon === "03d" || data.weather[0].icon === "03n"){
+            setWeatherIcon(drizzle_icon);
+        }
+        else if(data.weather[0].icon === "04d" || data.weather[0].icon === "04n"){
+            setWeatherIcon(drizzle_icon);
+        }
+        else if(data.weather[0].icon === "09d" || data.weather[0].icon === "09n"){
+            setWeatherIcon(rain_icon);
+        }
+        else if(data.weather[0].icon === "10d" || data.weather[0].icon === "10n"){
+            setWeatherIcon(rain_icon);
+        }   
+        else if(data.weather[0].icon === "13d" || data.weather[0].icon === "13n"){
+            setWeatherIcon(snow_icon);
+        }   
+        else {
+            setWeatherIcon(clear_icon);
         }
     };
 
@@ -52,25 +83,29 @@ export const WeatherApp = () => {
                 </div>     
         </div>
         <div className="weather-image">
-                <img src={clouds_icon} alt="cloud" className="cloud" />
+                <img src={weathericon} alt="cloud" className="cloud" />
         </div>
         <div className="weather-temp">
+            60°F
+        </div>
+        <div className="feels-like">
+            Feels like 59°F
         </div>
         <div className="weather-location">
-            <h1></h1>
+            New York
         </div>
         <div className="data-container">
             <div className="element">
                 <img src={humidity_icon} alt="" className="icon" />
                 <div className="data">
-                    <div className="humidity-percent"></div>
+                    <div className="humidity-percent">61%</div>
                     <div className="text">Humidity</div>
                 </div>
             </div>
             <div className="element">
                 <img src={wind_icon} alt="" className="icon" />
                 <div className="data">
-                    <div className="wind-rate"></div>
+                    <div className="wind-rate">8.05 km/hr</div>
                     <div className="text">Wind</div>
                 </div>
             </div>
